c37ae031df520caede220f9a52eb4fee868586cf
saloha10 <salah415@outlook.com>
2020-05-10 17:46:51 +0200

    ButtonArray
    En buttonarray som lägger in knapparna i en array och lägger ut dem i ett 3x3 fält

---
+++ src/spel/ButtonArray.java
@@ -0,0 +1,61 @@
+package spel;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class ButtonArray extends Application{
+	private Stage primary;
+	private Scene scene;
+	private GridPane gridpane;
+	//array för att lägga in 9 knappar
+	private Button [] buttons;
+	private final int antalButtons = 9;
+	
+	
+//launch args startar javafx applicationen genom "void start"
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	public void start(Stage arg0) throws Exception {
+		primary = arg0;
+		gridpane = new GridPane();
+		//width och height för scene 
+		scene = new Scene(gridpane, 300, 300);
+		
+		//for loop som anroper antalbuttons
+	buttons = new Button[antalButtons];
+		for (int i=0; i < antalButtons; ++i) {
+	//eftersom räkneordningen brukar alltid börja på 0 så plusade jag till en etta för att det ska gå från 1-9
+			buttons[i] = new Button("Button " + (i + 1));
+		//Storlek för knapparna	
+		    buttons[i].setPrefSize(100, 100);
+		}
+		
+		int buttonIndex = 0;
+		for (int rowIndex = 0; rowIndex < 3; ++rowIndex) {
+			for (int kolIndex = 0; kolIndex < 3; ++kolIndex) {
+				gridpane.add(buttons[buttonIndex], kolIndex, rowIndex);
+				++buttonIndex;
+			}
+			
+			
+		}
+		
+		
+		
+	
+		primary.setTitle("Tictactoe");
+		//lägger in scene inuti stage
+		primary.setScene(scene);
+		//ser till att staget visas
+		primary.show();
+		
+		
+	}
+
+}
